1ccc7f1cce15f4686923578f24e2cd63bf1b0b73
Karanveer04 <karanveer0426@gmail.com>
2018-02-28 13:27:34 +0100

    Basic Weather App

---
+++ weather.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+    $('#sendWeather').click(function () {
+
+       var city = $("#city").val();
+       const APPID = '07897dbfaeebe355c9948e5559b568b1';
+
+       if(city != ''){
+           $.ajax({
+               url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric" +
+               "&APPID=" + APPID + "",
+               type: "GET",
+               dataType: "jsonp",
+               success: function (data) {
+
+                   var info = displayWeather(data);
+
+                   $("#displayResult").html(info);
+
+                   $("#city").val('');
+               },
+               error: function () {
+                   $("#cityError").html("<div class='alert alert-danger text-center'><a href='#' class='close' data-dismiss='alert' " +
+                       "aria-label='close'>&times;</a>City Doesnt Exist!</div>");
+                   $("#city").val('');
+               }
+
+           });
+       } else{
+           $("#error").html("<div class='alert alert-danger text-center'><a href='#' class='close' data-dismiss='alert' " +
+               "aria-label='close'>&times;</a>Fields cannot be empty</div>");
+       }
+    });
+    // Implement error handling (upon 404 error).
+    // Maybe implement functionality to include geographical coordinates
+    // Put APPID as a variable
+    function displayWeather(data) {
+        if(data.cod !== 404) {
+
+            return "<h2 class='text-center'>Current Weather for " + data.name + ", " + data.sys.country + "</h2>" +
+                "<h3 style='padding-left: 130px;'><strong>Weather</strong>: " + data.weather[0].main + "</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Description</strong>: <img src='http://openweathermap.org/img/w/"
+                + data.weather[0].icon + ".png'> " + data.weather[0].description + "</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Temperature</strong>: " + data.main.temp + "&deg;C</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Pressure</strong>: " + data.main.pressure + " hPa</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Humidity</strong>: " + data.main.humidity + "%</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Minimum Temperature</strong>: " + data.main.temp_min + "&deg;C</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Maximum Temperature</strong>: " + data.main.temp_max + "&deg;C</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Wind Speed</strong>: " + data.wind.speed + " m/s</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Wind Direction</strong>: " + data.wind.deg + "&deg;</h3>" +
+                "<h3 style='padding-left: 130px;'><strong>Clouds</strong>: " + data.clouds.all + "%</h3>";
+        }
+    }
+
+})
